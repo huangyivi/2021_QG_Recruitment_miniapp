11e8439e9028921f976ba6823f4c92f99b206012
xhw2333 <1311844250@qq.com>
2021-02-08 14:53:41 +0800

    完成学生面试端功能（待测试）

--- utils/util.js
+++ utils/util.js
@@ -14,19 +14,36 @@ const formatNumber = n => {
   return n[1] ? n : `0${n}`
 }
 
-const getDom = (dom)=>{
-  return new Promise((resolve,reject)=>{
+const getDom = (dom) => {
+  return new Promise((resolve, reject) => {
     let query = wx.createSelectorQuery();
     query.select(dom).boundingClientRect();
     query.selectViewport().scrollOffset();
-    query.exec(function(res){
+    query.exec(function (res) {
       resolve(res[0]);
     })
 
   })
 }
 
+const formatDate = (date) => {
+  if(!date) date = new Date();
+  //date是传入的时间
+  let d = new Date(date);
+
+  let month = (d.getMonth() + 1) < 10 ? '0' + (d.getMonth() + 1) : (d.getMonth() + 1);
+  let day = d.getDate() < 10 ? '0' + d.getDate() : d.getDate();
+  let hours = d.getHours() < 10 ? '0' + d.getHours() : d.getHours();
+  let min = d.getMinutes() < 10 ? '0' + d.getMinutes() : d.getMinutes();
+  let sec = d.getSeconds() < 10 ? '0' + d.getSeconds() : d.getSeconds();
+
+  let times = d.getFullYear() + '-' + month + '-' + day + ' ' + hours + ':' + min + ':' + sec;
+
+  return times;
+}
+
 module.exports = {
   formatTime,
-  getDom
+  getDom,
+  formatDate
 }
